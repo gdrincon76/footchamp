5d0323dc43096d19cc5c8aed661ade95184ab499
grincon17 <grincon17@jaumebalmes.net>
2020-05-24 00:50:52 +0200

    añadido toolbar a activity inicial y marco de imagen de jugador

--- app/src/main/java/net/jaumebalmes/grincon17/futchamp/activities/LeaguesActivity.java
+++ app/src/main/java/net/jaumebalmes/grincon17/futchamp/activities/LeaguesActivity.java
@@ -1,6 +1,7 @@
 package net.jaumebalmes.grincon17.futchamp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
@@ -8,11 +9,17 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import net.jaumebalmes.grincon17.futchamp.R;
+import net.jaumebalmes.grincon17.futchamp.fragments.LoginDialogFragment;
 import net.jaumebalmes.grincon17.futchamp.interfaces.OnListLeagueInteractionListener;
+import net.jaumebalmes.grincon17.futchamp.interfaces.OnLoginDialogListener;
 import net.jaumebalmes.grincon17.futchamp.models.League;
 
 /**
@@ -20,16 +27,65 @@ import net.jaumebalmes.grincon17.futchamp.models.League;
  *
  * @author guillermo
  */
-public class LeaguesActivity extends AppCompatActivity implements OnListLeagueInteractionListener {
-
+public class LeaguesActivity extends AppCompatActivity implements OnListLeagueInteractionListener, OnLoginDialogListener {
+    LoginDialogFragment loginDialogFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leagues);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         // TODO -> Aquí se debería comprobar con Shared preference si hay un usuario autenticado.
      }
+    /**
+     * Este método crea el menú del toolbar
+     *
+     * @param menu el menú del sistema
+     * @return true para que muestre el menú
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO: implementar una condición si el usuario es coordinador y está logueado usar su menú,
+        //  en caso contrario cargar el menú de login
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_login_menu, menu);
+        return true;
+    }
 
+    /**
+     * Este método sirve para elegir un elemento del menú
+     *
+     * @param item los elementos del menú
+     * @return el padre
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_icon:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.account_login:
+                loginDialogFragment = new LoginDialogFragment();
+                loginDialogFragment.show(getSupportFragmentManager(), getString(R.string.login_txt));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Es la implementación del click del login de la ventana flotante
+     *
+     * @param userName el nombre de usuario introducido en el campo de TextEdit
+     * @param pwd      la contraseña introducida en el campo de TextEdit
+     */
+    @Override
+    public void onLoginClickListener(String userName, String pwd) {
+        // TODO: aquí se debe implementar la llamada a la api para comprobar que el usuario se loguea,
+        //  guardarlo en el shared preferences cargar el nuevo menú de usuario autenticado
+        Toast.makeText(this, "Name: " + userName + "pwd: " + pwd, Toast.LENGTH_LONG).show();
+    }
 
     /**
      * Este método es la implementación de la interfaz OnListJugadorInteractionListener
