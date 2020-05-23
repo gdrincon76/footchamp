e3813982e146cda759763481d8e037be44c60ace
Tomas Estrada <tomasenriquea@hotmial.com>
2020-05-23 16:27:16 +0200

    Se crean las clases POJO Calendario y Partido y sus reposritorios API, se crea los metodo para obtener la lista de jugadores de un equipo por medio de su nombre y el metodo para obtener las lista de partidos de un equipo buscado tambien por su nombre. falta implementar la llegada de datos.

---
+++ app/src/main/java/net/jaumebalmes/grincon17/futchamp/models/Partido.java
@@ -0,0 +1,56 @@
+package net.jaumebalmes.grincon17.futchamp.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Partido implements Serializable {
+
+    private Long id;
+    private LocalDate fecha; // Fecha del partido
+    private LocalTime hora; // Hora de inicio
+    private int jornada; // sera el numero de la jornada en donde se encuentra el partido
+    private Equipo local;  // id de equipo y otros datos
+    private Equipo visitante; // id de equipo y otros datos
+    private Calendario calendario; // id calendario que genero los partidos
+
+    // FALTAN HACER 1 CAMPO ==>> Marcador ==> Falta terminar
+
+    //Constructores
+    public Partido() {
+    }
+
+
+    // Setter y Getter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public int getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(int jornada) {
+        this.jornada = jornada;
+    }
+}
--- app/src/main/java/net/jaumebalmes/grincon17/futchamp/repositoryApi/JugadorRepositoryApi.java
+++ app/src/main/java/net/jaumebalmes/grincon17/futchamp/repositoryApi/JugadorRepositoryApi.java
@@ -6,10 +6,19 @@ import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JugadorRepositoryApi {
 
     // Obtiene la lista de jugadores de la BBDD por medio de la API
     @GET("mostrar")
     Call<ArrayList<Jugador>> obtenerListaJugadores();
+
+
+    // Obtiene la lista de jugadores de un mismo equipo por medio del nombre de este
+    @GET("mostrar/equipo/{nombreEquipo}")
+    Call<ArrayList<Jugador>> obtenerListaJugadoresEquipo(@Path("nombreEquipo") String nombreEquipo);
+
+
+
 }
