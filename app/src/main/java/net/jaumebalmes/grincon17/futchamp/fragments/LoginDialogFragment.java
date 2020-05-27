2c27b5c26919aa964515ec5d8d1b2047529c0689
grincon17 <grincon17@jaumebalmes.net>
2020-05-28 00:16:14 +0200

    solución a conflicto de versiones

--- app/src/main/java/net/jaumebalmes/grincon17/futchamp/fragments/LoginDialogFragment.java
+++ app/src/main/java/net/jaumebalmes/grincon17/futchamp/fragments/LoginDialogFragment.java
@@ -6,10 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
-<<<<<<< HEAD
-=======
-import android.util.Log;
->>>>>>> 99e8e44a9fe9a9bc201a47769d481cbd77a6c3e0
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -30,8 +26,6 @@ public class LoginDialogFragment extends DialogFragment {
     private EditText userName;
     private EditText pwd;
 
-    private Retrofit retrofitCoordinador;
-
     @SuppressLint("InflateParams")
     @NonNull
     @Override
@@ -49,12 +43,8 @@ public class LoginDialogFragment extends DialogFragment {
                 // Obtiene los datos del usuario para verificar autorizacion
                 String name = String.valueOf(userName.getText());
                 String pass = String.valueOf(pwd.getText());
-
                 if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(pass)) {
-                    // mListener.onLoginClickListener(name, pass); // Muestra mensaj
-                    verificarAutorizacionUsuario(name, pass);   // Verifica la respuesta de seguridad
-
-                    mListener.onLoginClickListener(name, pass); // Muestra mensaje
+                    mListener.onLoginClickListener(name, pass);
                 } else {
                     Toast.makeText(getContext(), "Must not be empty " + name, Toast.LENGTH_SHORT).show();
                 }
@@ -77,38 +67,4 @@ public class LoginDialogFragment extends DialogFragment {
                     + " must implement NoticeDialogListener");
         }
     }
-
-    // =============================================================================================
-    // CONEXION A LA API
-
-    // Este metodo verifica la respuesta de seguridad obteniendo un valor de tipo Boleano.
-    private void verificarAutorizacionUsuario(String name, String pass) {
-        CoordinadorRepositoryApi coordinadorRepositoryApi = retrofitCoordinador.create(CoordinadorRepositoryApi.class);
-        Call<Boolean> verificandoRespuesta = coordinadorRepositoryApi.verificarAutorizacion(name, pass);
-
-        verificandoRespuesta.enqueue(new Callback<Boolean>() {
-            @Override
-            public void onResponse(Call<Boolean> call, @NonNull Response<Boolean> response) {
-                if (response.isSuccessful()) {
-                    Boolean respuesta; // Para almacenar la respuesta de seguridad para el acceso de un coordinador
-                    respuesta = response.body(); // Obtiene la respuesta para saber si el coordinador tiene acceso.
-
-                    // Aqui se puede ver la respuesta y trabajar con ella
-                    Log.e(TAG, " RESPUESTA DE SEGURIDAD: " + respuesta);
-                } else {
-                    try { // Esta respuesta solo se muetra si el valor es false
-                        Log.e(TAG, " NO TIENE AUTORIZACION: onResponse: " + response.errorBody().string());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Boolean> call, Throwable t) {
-                Log.e(TAG, " => ERROR VERIFICAR LA CONEXION => onFailure: " + t.getMessage());
-            }
-        });
-    }
-
 }
